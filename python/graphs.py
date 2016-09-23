045cfe1cdaf5041673573323edbad660cf1db052
Daniel Dahlmeier <d.dahlmeier@sap.com>
2016-09-23 16:34:15 +0800

    dijkstra

--- python/graphs.py
+++ python/graphs.py
@@ -7,6 +7,7 @@ import numpy as np
 import random
 from itertools import chain
 from operator import itemgetter
+import heapq
 
 
 def unique(iter):
@@ -122,6 +123,45 @@ def breath_first_search(graph, start, end):
                    if not visited[neighbour]):
             queue.append(to)
     return False
+
+
+def trace_back(graph, backpointers, node):
+    """ return path from node through backpointers"""
+    path = []
+    while node:
+        path.append(node)
+        node = backpointers.get(node)
+    return list(reversed(path))
+
+
+def dijkstra(graph, start, end):
+    """Dijkstra shortest-path search"""
+    queue = [(0, start)]  # priority queue for nodes
+    distance = {n: np.inf for n in graph.nodes()}  # shortest distance to nodes
+    distance[start] = 0
+    visited = {n: False for n in graph.nodes()}
+    backpointers = {}  # store back pointers for tracing back path
+    print "dijkstra search", start, "->", end
+    while queue:
+        dist, node = queue.pop(0)
+        # ignore visited nodes, can double visit due to queue duplicates
+        if visited[node]:
+            continue
+        print "visit", node, " with distance", dist
+        visited[node] = True
+        if node is end:
+            return (dist, trace_back(graph, backpointers, node))
+        for to in (neighbour for neighbour in graph.adjacent(node)
+                   if not visited[neighbour]):
+            print "check", node, " ->", to
+            # update distance if we have found a new shortest path
+            if distance[node] + graph.get_edge(node, to) < distance[to]:
+                print "update shortest path"
+                distance[to] = distance[node] + graph.get_edge(node, to)
+                backpointers[to] = node
+            # add neighbour to queue
+            heapq.heappush(queue, (distance[to], to))
+    return (np.inf, [])
 
 
 def random_graph(Graph, n_nodes, n_edges):
